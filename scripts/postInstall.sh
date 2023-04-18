69e0c640b9cbf923bd85d125bc2b428dd8e1fce0
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-04-18 16:05:56 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,4 +3,14 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-# sleep 30s;
+sleep 15s;
+
+target=$(docker-compose port memos 5230)
+
+curl http://${target}/api/auth/signup \
+  -H 'accept: application/json, text/plain, */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'content-type: application/json' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36' \
+  --data-raw '{"username":"admin","password":"'${ADMIN_PASSWORD}'"}' \
+  --compressed
